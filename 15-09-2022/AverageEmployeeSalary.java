390028daeed07dc9bf2bf0530b44c31524985018
prathamm18 <112954385+prathamm18@users.noreply.github.com>
2022-09-15 18:29:57 +0530

    Add files via upload

---
+++ 15-09-2022/AverageEmployeeSalary.java
@@ -0,0 +1,14 @@
+class AverageEmployeeSalary 
+{
+
+	public static void main(String[]args)
+	{
+		int salary = 10000;
+		int a=11000,b=5000;
+		double c;
+
+		c =(salary*3) - (a + b);
+
+		System.out.println("Salary of 3rd Employee is:"+ c);
+	}
+}
